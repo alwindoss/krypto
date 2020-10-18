edf18e29b8c6b20f35a661967a41b847577de9bc
Alwin Doss <alwindoss84@gmail.com>
2020-10-18 19:33:01 +0530

    moved from DH interface to struct, made the API better with error
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- dh/dh.go
+++ dh/dh.go
@@ -1,6 +1,7 @@
 package dh
 
 import (
+	"errors"
 	"math/big"
 )
 
@@ -16,15 +17,6 @@ const (
 	GroupSeventeen Group = 17
 	GroupEighteen  Group = 18
 )
-
-// DH defines the interface for Diffie-Hellman operations
-type DH interface {
-	PVal() *big.Int
-	GVal() *big.Int
-	Generate()
-	PublicKey() *big.Int
-	SecretKey(*big.Int) *big.Int
-}
 
 /*
 From the RFC 3526: https://datatracker.ietf.org/doc/rfc3526/?include_text=1
@@ -54,16 +46,16 @@ const (
 )
 
 // New creates a new DH
-func New(gp Group, privKey *big.Int) DH {
+func New(gp Group, privKey *big.Int) (*DH, error) {
 	// var p = new(big.Int)
 	// var g = new(big.Int)
-	dhObj := &dh{}
+	dhObj := &DH{}
 	switch gp {
 	case GroupFive:
 		p, ok := new(big.Int).SetString(gpFiveP, 16)
 		g := new(big.Int).SetInt64(2)
 		if !ok {
-			panic("unable to create the P value")
+			return nil, errors.New("unable to create the P value")
 		}
 		dhObj.P = p
 		dhObj.G = g
@@ -72,7 +64,7 @@ func New(gp Group, privKey *big.Int) DH {
 		p, ok := new(big.Int).SetString(gpFourteenP, 16)
 		g := new(big.Int).SetInt64(2)
 		if !ok {
-			panic("unable to create the P value")
+			return nil, errors.New("unable to create the P value")
 		}
 		dhObj.P = p
 		dhObj.G = g
@@ -81,7 +73,7 @@ func New(gp Group, privKey *big.Int) DH {
 		p, ok := new(big.Int).SetString(gpFifteenP, 16)
 		g := new(big.Int).SetInt64(2)
 		if !ok {
-			panic("unable to create the P value")
+			return nil, errors.New("unable to create the P value")
 		}
 		dhObj.P = p
 		dhObj.G = g
@@ -90,7 +82,7 @@ func New(gp Group, privKey *big.Int) DH {
 		p, ok := new(big.Int).SetString(gpSixteenP, 16)
 		g := new(big.Int).SetInt64(2)
 		if !ok {
-			panic("unable to create the P value")
+			return nil, errors.New("unable to create the P value")
 		}
 		dhObj.P = p
 		dhObj.G = g
@@ -99,7 +91,7 @@ func New(gp Group, privKey *big.Int) DH {
 		p, ok := new(big.Int).SetString(gpSeventeenP, 16)
 		g := new(big.Int).SetInt64(2)
 		if !ok {
-			panic("unable to create the P value")
+			return nil, errors.New("unable to create the P value")
 		}
 		dhObj.P = p
 		dhObj.G = g
@@ -108,17 +100,18 @@ func New(gp Group, privKey *big.Int) DH {
 		p, ok := new(big.Int).SetString(gpEighteenP, 16)
 		g := new(big.Int).SetInt64(2)
 		if !ok {
-			panic("unable to create the P value")
+			return nil, errors.New("unable to create the P value")
 		}
 		dhObj.P = p
 		dhObj.G = g
 		dhObj.PrivKey = privKey
 
 	}
-	return dhObj
+	return dhObj, nil
 }
 
-type dh struct {
+// DH is the datastructure the stores all the accessible information in Diffie-Hellman
+type DH struct {
 	P       *big.Int
 	G       *big.Int
 	PrivKey *big.Int
@@ -126,27 +119,32 @@ type dh struct {
 	SecKey  *big.Int
 }
 
-func (d *dh) PVal() *big.Int {
+// PVal retries the P value after the initial set up. Call this method only after New()
+func (d *DH) PVal() *big.Int {
 	return d.P
 }
 
-func (d *dh) GVal() *big.Int {
+// GVal retries the P value after the initial set up. Call this method only after New()
+func (d *DH) GVal() *big.Int {
 	return d.G
 
 }
 
-func (d *dh) Generate() {
+// Generate generates the public key that can be shared across the network
+func (d *DH) Generate() error {
 	pub := new(big.Int).Exp(d.G, d.PrivKey, d.P)
 	d.PubKey = pub
-
+	return nil
 }
 
-func (d *dh) PublicKey() *big.Int {
+// PublicKey retrieves the public key that was generated using Generate() method
+func (d *DH) PublicKey() *big.Int {
 	return d.PubKey
 }
 
-func (d *dh) SecretKey(pubKey *big.Int) *big.Int {
+// SecretKey generates the secret given the public key
+func (d *DH) SecretKey(pubKey *big.Int) (*big.Int, error) {
 	secKey := new(big.Int).Exp(pubKey, d.PrivKey, d.P)
 	d.SecKey = secKey
-	return secKey
+	return secKey, nil
 }
--- dh/dh_test.go
+++ dh/dh_test.go
@@ -10,14 +10,20 @@ func Test(t *testing.T) {
 	BobPrivKey, _ := new(big.Int).SetString("89898989", 16)
 	// AlicePrivKey, _ := new(big.Int).SetString("87e0beefd8122561e9c329d764c6e3b3dafe538a", 16)
 	// BobPrivKey, _ := new(big.Int).SetString("4fc9904887ac7fabff87f054003547c2d9458c1f6f584c140d7271f8b266bb390af7e3f625a629bec9c6a057a4cbe1a556d5e3eb2ff1c6ff677a08b0c7c509110b9e7c6dbc961ca4360362d3dbcffc5bf2bb7207e0a5922f77cf5464b316aa49fb62b338ebcdb30bf573d07b663bb7777b69d6317df0a4f636ba3d9acbf9e8ac", 16)
-	alice := New(GroupFourteen, AlicePrivKey)
+	alice, err := New(GroupFourteen, AlicePrivKey)
+	if err != nil {
+		t.FailNow()
+	}
 	alice.Generate()
 	alicePubKey := alice.PublicKey()
-	bob := New(GroupFourteen, BobPrivKey)
+	bob, err := New(GroupFourteen, BobPrivKey)
+	if err != nil {
+		t.FailNow()
+	}
 	bob.Generate()
 	bobPubKey := bob.PublicKey()
-	bobSecretKey := bob.SecretKey(alicePubKey)
-	aliceSecretKey := alice.SecretKey(bobPubKey)
+	bobSecretKey, _ := bob.SecretKey(alicePubKey)
+	aliceSecretKey, _ := alice.SecretKey(bobPubKey)
 	if bobSecretKey.String() != aliceSecretKey.String() {
 		t.FailNow()
 	}
